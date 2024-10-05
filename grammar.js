7b2e1a47d5ff6b6c5fa5bbf79d41eac2dbe35ae3
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-10-05 15:33:00 -0400

    add more shit

--- grammar.js
+++ grammar.js
@@ -4,25 +4,57 @@
 module.exports = grammar({
   name: "lit",
   word: $ => $.text_identifier,
+  supertypes: $ => [$._statement, $._pat, $._expression, $._type],
+  conflicts: $ => [[$.block, $.block_expr]],
 
   rules: {
     // TODO: add the actual grammar rules
     source_file: $ => seq(optional($.shebang), repeat(seq($._statement))),
     shebang: $ => seq(/#!/, field("program", $.nonword), optional(field("argument", $.nonword))),
-    _statement: $ => choice(
-      $.let_statement,
-    ),
-    let_statement: $ => seq("let", field("name", $._identifier), optional(seq(":", field("type", $._type))), optional(seq("=", field("value", $._expression)))),
 
-    // expressions
+    //{{{1 statements
+    _statement: $ => choice(
+      $.expr_stateent,
+      $.if_statemnt,
+      $.let_statement,
+      $.fun_statement,
+      $.block,
+      $.fake_stmt,
+    ),
+    expr_stateent: $ => seq($._expression, ";"),
+    if_statemnt: $ => seq("if", field("condition", $._expression), choice(field("body", $.block), seq(":", field("body", $._statement)))),
+    let_statement: $ => seq("let", field("name", $._identifier), optional(seq(":", field("type", $._type))), optional(seq("=", field("value", $._expression)))),
+    fun_statement: $ => seq("fun", field("name", $._identifier), $._fun_rhs),
+    block: $ => seq("{", field("body", repeat($._statement)), optional(field("return", $._expression)), "}"),
+    fake_stmt: $ => "?stmt?",
+
+
+    //{{{1 functions
+    _fun_rhs: $ => seq("(", field("args", optional(seq($.fun_arg, repeat(seq(",", $.fun_arg))))), ")"),
+    fun_arg: $ => $._pat,
+
+    //{{{1 patterns
+    _pat: $ => choice(
+      $.ident_pat,
+      $.type_pat,
+      $.fake_pat,
+    ),
+    ident_pat: $ => $._identifier,
+    type_pat: $ => seq($._pat, ":", $._type),
+    fake_pat: $ => "?pat?",
+
+    //{{{1 expressions
     _expression: $ => choice(
       $._constant,
       $.block_expr,
       $.fun_expr,
       $.fun_call,
       $._binop,
-      // seq("(", $._expression, ")"),
+      $.paren_expr,
+      $.fake_expr,
     ),
+    fake_expr: $ => "?expr",
+    paren_expr: $ => seq(/\(/, $._expression, /\)/),
     block_expr: $ => seq("{", field("body", repeat($._statement)), optional(field("return", $._expression)), "}"),
     fun_expr: $ => "_",
     fun_call: $ => "__",
@@ -32,7 +64,7 @@ module.exports = grammar({
     mul: $ => prec.left(2, seq(field("left", $._expression), choice("*", "×"), field("right", $._expression))),
     div: $ => prec.left(2, seq(field("left", $._expression), choice("/", "÷"), field("right", $._expression))),
 
-    // typesystem
+    //{{{1 types
     _type: $ => choice(
       $.union_type,
       $.add_type,
@@ -41,19 +73,22 @@ module.exports = grammar({
       $.array_type,
       $.tuple_type,
       $._constant,
-      seq("(", $._type, ")"),
+      $.paren_type,
+      $.fake_type,
     ),
+    paren_type: $ => seq("(", $._type, ")"),
     union_type: $ => prec.left(4, seq($._type, "||", $._type)),
     add_type: $ => prec.left(3, seq($._type, "+", $._type)),
     array_type: $ => seq("[", $._type, "]"),
     applied_type: $ => seq(field("name", $._identifier), optional(seq("(", optional(field("args", $._type_args)), ")"))),
     tuple_type: $ => seq("(", optional(seq($._type, ",", optional(seq($._type, repeat(seq(",", $._type)))))), ")"),
     primitive_type: $ => choice("number", "string", "boolean"),
+    fake_type: $ => "?type",
     _identifier: $ => choice($.text_identifier, $.emoji_identifier),
     _identifier_with_colon: $ => choice(seq($.text_identifier, ":"), seq($.emoji_identifier, optional(":"))),
-    _type_args: $ => seq(choice($._type, "~"), repeat(seq(",", choice($._type, "~")))),
+    _type_args: $ => choice($.vararg, seq($._type, repeat(seq(",", $._type)), optional(seq(",", $.vararg)))),
 
-    // constants — acceptable as both types and values
+    //{{{1 constants — acceptable as both types and values
     _constant: $ => choice(
       $.number,
       $.boolean,
@@ -65,9 +100,12 @@ module.exports = grammar({
     boolean: $ => choice("true", "false"),
     nil: $ => "nil",
 
-    // pieces
+    //{{{1 pieces
     nonword: $ => /\S+/,
+    vararg: $ => "...",
     text_identifier: $ => /\p{XID_Start}\p{XID_Continue}*/,
     emoji_identifier: $ => /\p{Emoji}/,
+
+    //}}}1
   }
 });
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,17 +1,41 @@
 (let_statement
-	"let" @keyword
-	name: (_) @attribute)
-":" @operator
-"=" @operator
-"+" @operator
-"-" @operator
-"/" @operator
-"*" @operator
-"×" @operator
-"÷" @operator
+  "let" @keyword
+  name: (_) @attribute
+  ":"? @punctuation.delimiter
+  "="? @punctuation.delimiter
+  )
+(if_statemnt
+  "if" @keyword
+  ":"? @punctuation.delimiter)
+(block
+  "{" @punctuation.bracket
+  "}" @punctuation.bracket)
+(block_expr
+  "{" @punctuation.bracket
+  "}" @punctuation.bracket)
+(add "+" @operator)
+(sub "-" @operator)
+(div "/" @operator)
+(mul "*" @operator)
+(mul "×" @operator)
+(div "÷" @operator)
+(fake_stmt) @constant
+(fake_pat) @constant
+(fake_expr) @constant
+(fake_type) @constant
 (primitive_type) @type.builtin
 (applied_type
-  name: (_) @type)
+  name: (_) @type
+  "(" @punctuation.bracket
+  "," @punctuation.delimiter
+  ")" @punctuation.bracket)
+(array_type
+  "[" @punctuation.bracket
+  "]" @punctuation.bracket)
+(tuple_type
+  "(" @punctuation.bracket
+  "," @punctuation.delimiter
+  ")" @punctuation.bracket)
 (union_type "||" @operator)
 (add_type "+" @operator)
 (ERROR) @error
@@ -19,11 +43,4 @@
 (string) @string
 (boolean) @variable.builtin
 (number) @number
-"(" @punctuation.bracket
-")" @punctuation.bracket
-"[" @punctuation.bracket
-"]" @punctuation.bracket
-"," @punctuation.delimiter
 (shebang program: _ @type.builtin argument: _? @string) @punctuation.delimiter
-; "{" @punctuation.bracket
-; "}" @punctuation.bracket
